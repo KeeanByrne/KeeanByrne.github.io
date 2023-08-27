c2ed3ba4d0362d323df0cc55a2af2c8683039076
KeeanByrne <keeanbyrne00@gmail.com>
2023-08-27 20:09:22 +0200

    Big commit

---
+++ src/components/Banner.js
@@ -0,0 +1,63 @@
+import { useState, useEffect } from "react";
+import { Col, Container, Row } from "react-bootstrap";
+import { ArrowUpRightCircle } from "react-bootstrap-icons";
+import headerImg from '../assets/img/header-img.svg';
+
+export const Banner = () => {
+    const [loopNum, setLoopNum] = useState(0);
+    const [isDeleting, setIsDeleting] = useState(false);
+    const toRotate = ["Software Developer", "Android Developer", "Beginner Web Designer", "Golfer"];
+    const [text, setText] = useState('');
+    const [delta, setDelta] = useState(300 - Math.random() * 100 );
+    const period = 2000;
+
+    useEffect(() => {
+        let ticker = setInterval(() => {
+            tick();
+        },delta)
+
+        return () => { clearInterval(ticker)};
+    }, [text])
+    
+
+    const tick = () => {
+        let i = loopNum % toRotate.length;
+        let fullText = toRotate[i];
+        let updatedText = isDeleting ? fullText.substring(0, text.length -1) : fullText.substring(0, text.length + 1);
+
+
+        setText(updatedText);
+
+        if(isDeleting){
+            setDelta(prevDelta => prevDelta /2)
+        }
+
+        if(!isDeleting && updatedText === fullText){
+            setIsDeleting(true);
+            setDelta(period);
+        }else if(isDeleting && updatedText === ''){
+            setIsDeleting(false);
+            setLoopNum(loopNum + 1);
+            setDelta(500);
+        }
+    }
+
+    return(
+        <section className="banner" id="home">
+            <Container>
+                <Row className="align-items-center">
+                    <Col xs={12} md={6} xl={7}>
+                        <span className="tagline">Welcome To My Portfolio</span>
+                        <h1>{'Hi Im Keean '}<span className="wrap">{ text }</span></h1>
+                        <p>I am currently completing my final year at Varsity College where I am studying a Bachelor of Computer and Information Sciences in Application Development. My objective is to further enhance my skills and knowledge whilst diving into the creative world of software development. In my current free time I am either working on various side projects (such as my portfolio website, coming soon…) or perfecting my golf swing on the course.  </p>
+                        <button onClick={() => console.log('connect')}>Lets Connect <ArrowUpRightCircle size={25} /></button>
+                    </Col>
+                    <Col xs={12} md={6} xl={5}>
+                        <img src={headerImg} alt="Header Img" />
+                    </Col>
+                </Row>
+            </Container>
+        
+        </section>
+    )
+}
